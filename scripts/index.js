eb88a81544057d8bab89e69855ede87e92f20801
Evelina Melnikova <melnikovaa45@yandex.ru>
2023-07-11 06:40:59 +0300

    start

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,18 @@
-console.log("скрипт подключен");
 
 const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.info__edit-button');
-const popupSave = document.querySelector ('.popup__save-button');
+const popupOpenButton = document.querySelector('.profile__info-edit-button');
 
 const popupCloseButton = document.querySelector('.popup__close-button');
-let Name = document.querySelector('.info__name');
-let Job = document.querySelector('.info__job');
+let Name = document.querySelector('.profile__info-name');
+let Job = document.querySelector('.profile__info-popup-job');
 
-const formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+const formElement = document.querySelector('.name');
+let nameInput = document.querySelector('.popup__name_value');
+let jobInput = document.querySelector('.popup__job_value');
+
+const popupToggle = function (){
+    popup.classList.toggle('popup_opened'); 
+}
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -18,7 +20,10 @@ function handleFormSubmit(evt) {
     let JobValue = jobInput.value;
     Name.textContent = NameValue;
     Job.textContent = JobValue;
+    popup.classList.toggle('popup_opened');
 }
+
+
 
 popupOpenButton.addEventListener("click", function() {
     popup.classList.toggle('popup_opened');
@@ -30,10 +35,8 @@ popupCloseButton.addEventListener("click", function(){
     popup.classList.toggle('popup_opened');
 }) ;
 
-popupSave.addEventListener("click", function(){
-    popup.classList.toggle('popup_opened');
 
-});
+
 formElement.addEventListener('submit', handleFormSubmit);
 
 
